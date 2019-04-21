39fd3586436403143e7b06612ffa0dd3a41d848e
shootk <shoot.k.uta.7@gmail.com>
2019-04-21 15:44:31 +0900

    ok

--- python_practice/challenge21.py
+++ python_practice/challenge21.py
@@ -16,7 +16,8 @@ class Stack:
 
     def size(self):
         return len(self.items)
-
+#1
+"""
 s = "& i 0 u"
 word = Stack()
 for w in s:
@@ -25,3 +26,14 @@ s=""
 while word.size() >0:
     s += str(word.pop())
 print(s)
+"""
+
+#2
+num = [1,2,3,4,5]
+num_r = []
+s = Stack()
+for n in num:
+    s.push(n)
+for i in range(len(num)):
+    num_r.append(s.pop())
+print(num_r)
